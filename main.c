d3492816812ae46e8f907a27db8161fbe7d7862a
Pranav Arun <pranavarun2007@gmail.com>
2026-06-09 13:43:39 +0530

    Added circle and triangle drawing functionality

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 #define ROWS 20
 #define COLS 50
 
@@ -50,18 +51,55 @@ void drawRectangle(int x,int y,int width,int height)
 
 void drawLine(int x1,int y1,int x2,int y2)
 {
+    int dx=abs(x2-x1);
+    int dy=abs(y2-y1);
+
+    int steps=(dx>dy)?dx:dy;
+
+    float xInc=(float)(x2-x1)/steps;
+    float yInc=(float)(y2-y1)/steps;
+
+    float x=x1;
+    float y=y1;
+
     int i;
 
-    if(x1==x2)
+    for(i=0;i<=steps;i++)
     {
-        for(i=y1;i<=y2;i++)
-            canvas[x1][i]='*';
-    }
+        if((int)x>=0 && (int)x<ROWS &&
+           (int)y>=0 && (int)y<COLS)
+        {
+            canvas[(int)x][(int)y]='*';
+        }
 
-    else if(y1==y2)
+        x+=xInc;
+        y+=yInc;
+    }
+}
+
+void drawTriangle(int x1,int y1,
+                  int x2,int y2,
+                  int x3,int y3)
+{
+    drawLine(x1,y1,x2,y2);
+    drawLine(x2,y2,x3,y3);
+    drawLine(x3,y3,x1,y1);
+}
+
+void drawCircle(int xc,int yc,int r)
+{
+    int x,y;
+
+    for(x=0;x<ROWS;x++)
     {
-        for(i=x1;i<=x2;i++)
-            canvas[i][y1]='*';
+        for(y=0;y<COLS;y++)
+        {
+            int dx=x-xc;
+            int dy=y-yc;
+
+            if(dx*dx + dy*dy <= r*r)
+                canvas[x][y]='*';
+        }
     }
 }
 
@@ -75,9 +113,11 @@ int main()
     {
         printf("\n1.Rectangle");
         printf("\n2.Line");
-        printf("\n3.Display");
-        printf("\n4.Clear");
-        printf("\n5.Exit");
+        printf("\n3.Circle");
+        printf("\n4.Triangle");
+        printf("\n5.Display");
+        printf("\n6.Clear");
+        printf("\n7.Exit");
 
         printf("\nChoice: ");
         scanf("%d",&choice);
@@ -107,10 +147,36 @@ int main()
             }
 
             case 3:
+            {
+                int xc,yc,r;
+
+                printf("Enter center_x center_y radius: ");
+                scanf("%d%d%d",&xc,&yc,&r);
+
+                drawCircle(xc,yc,r);
+                break;
+            }
+
+            case 4:
+            {
+                int x1,y1,x2,y2,x3,y3;
+
+                printf("Enter x1 y1 x2 y2 x3 y3: ");
+
+                scanf("%d%d%d%d%d%d",
+                    &x1,&y1,
+                    &x2,&y2,
+                    &x3,&y3);
+
+                drawTriangle(x1,y1,x2,y2,x3,y3);
+                break;
+            }
+
+            case 5:
                 displayCanvas();
                 break;
 
-            case 4:
+            case 6:
                 clearCanvas();
                 break;
         }
